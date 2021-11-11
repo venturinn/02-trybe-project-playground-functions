d4113643b0dd2873e63f3662b2ca36a5963055dd
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-11 17:18:37 -0300

    Função 06 adicionada.

--- src/challenges.js
+++ src/challenges.js
@@ -21,35 +21,61 @@ function calcArea(base, height) {
 function splitSentence(a) {
 
     let splitString = a.split(" ");
-
     return splitString
 
 }
-
-
 
 // Desafio 4
 function concatName(a) {
 
     let first = a[0];
     let last = a[a.length - 1];
-
     let lastFist = (last + ', ' + first);
 
     return lastFist
-
 }
 
 
 // Desafio 5
-function footballPoints() {
-    // seu código aqui
+function footballPoints(wins, ties) {
+
+    let points = ((wins * 3) + ties);
+
+    return points
 }
 
 // Desafio 6
 function highestCount() {
-    // seu código aqui
+
+    // Descobrindo o maior valor do array:
+
+    let highestValue = 0;
+    let highestPosition = 0;
+    for (let index = 0; index < a.length; index += 1) {
+        if (a[index] >= a[highestPosition]) {
+            highestValue = a[index];
+            highestPosition = index;
+        }
+    }
+    // Descobrindo as repetições do maior valor:
+
+    let countRepetitions = 0;
+    for (let index2 = 0; index2 < a.length; index2 += 1) {
+        if (a[index2] === highestValue) {
+            countRepetitions = countRepetitions + 1;
+        }
+    }
+
+    return countRepetitions;
+
 }
+
+a = [9, 15, 2, 3, 9, 15, 7, 9, 15];
+
+highestCount(a);
+
+
+
 
 // Desafio 7
 function catAndMouse() {
