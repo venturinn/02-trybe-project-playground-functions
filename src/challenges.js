41ec7019a8f80cfd02d476cab79331154eb053a6
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-11 14:56:53 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket::rocket::rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -1,60 +1,61 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(1) {
+    // seu código aqui
 }
 
 // Desafio 2
 function calcArea() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 5
 function footballPoints() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+    // seu código aqui
 }
+
 function decode() {
-  // seu código aqui
+    // seu código aqui
 }
 
 module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    highestCount,
+    splitSentence,
 };
